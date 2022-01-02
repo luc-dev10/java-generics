82b057c617e200bda3c020e5f8a9a9b4170b0ba7
luc-dev10 <86529332+luc-dev10@users.noreply.github.com>
2022-01-02 10:21:06 -0500

    Initial commit

---
+++ src/com/generic/demo/Car.java
@@ -0,0 +1,20 @@
+package com.generic.demo;
+
+public class Car {
+
+	private String type;
+
+	public Car(String type) {
+		super();
+		this.type = type;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+}
